11ced48b53b43da5b4b754796b5331b13cb777af
donmatanon <donmatanon@gmail.com>
2017-10-05 12:46:59 +0300

    New commit with new file

--- webscript.js
+++ webscript.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         
 
         var url = ""; // the script where you handle the form input.
-
+       
 
             $.ajax({
                    type: "POST",
@@ -14,10 +14,30 @@ $(document).ready(function(){
                    success: function(data)
                    {
                        alert(data); // show response from the python server.
-                   }
+                       
+                        //succsfull connection
+                        if(data == 'success'){
+                            alert('data is success');
+                            window.location.replace("http://localhost/myMainApp.html");
+                        }
+                        //bad connection 
+                        else if(data == 'failed'){
+                            alert('Worng Username Password');
+                            $('#loginpop').show()
+                            $('#username').val('');
+                            $('#pwd').val('');
+                            
+                        }
+                        //db is not connectig
+                        else{
+                            alert('all is bad');
+                        }
+                            
+                    }
                  });
-        
                  event.preventDefault(); // avoid to execute the actual submit of the form.
+        
+                 
 
         
         });
